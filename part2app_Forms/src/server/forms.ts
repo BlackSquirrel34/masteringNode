9bc037facc96649d63affc187e47bb9b87de87b4
Edith <edith-friedgund-johanna.foerster@stud.uni-bamberg.de>
2024-10-02 12:38:18 +0200

    forms chapter init

---
+++ part2app_Forms/src/server/forms.ts
@@ -0,0 +1,7 @@
+import { Express } from "express";
+export const registerFormMiddleware = (app: Express) => {
+    // no middleware yet
+}
+export const registerFormRoutes = (app: Express) => {
+    // no routes yet
+}
